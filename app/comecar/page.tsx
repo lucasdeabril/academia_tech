cfc03873d25beafc5d0ad10953e30d443f368047
lucasdeabril <lucaspassosdesouza08@gmail.com>
2023-09-13 23:25:39 -0400

    nova rota comecar

--- app/comecar/page.tsx
+++ app/comecar/page.tsx
@@ -3,8 +3,27 @@ import banner from './fotos/banner.jpg'
 
 export default function Home() {
   return (
-    <main>
-        <h1 className='comecar'>hello word!</h1>
+    <main className='corpo'>
+        
+        <header>
+        <h1>Academia Tech</h1>
+        <div className='sub-header flex spacearound'>
+          <input type="button" className='inv' value={'Seja franqueado'} />
+          <input type="button" className='inv' value={'Academias'} />
+          <input type="button" className='inv' value={'Espaço do cliente'} />
+          <a href="./comecar"><input type="button" className='comecar inv' value={'Começar agora'} /></a>
+          <input type="button" value={'Menu'} />
+        </div>
+        
+      </header>
+
+      <div className='width h1comecar'>
+        <h1 className='h1'><b>E aí, onde quer treinar?</b></h1>
+      </div>
+
+      <div className='cep width'>
+        <input type="text" placeholder='Insira seu cep' />
+      </div>
     </main>
     )
 }
